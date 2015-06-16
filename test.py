36d1f1dbdff0efbb576d94e1d5a6a22bacbb67e3
unknown <MJTener@MAX.tcs.local>
2015-06-16 16:06:14 -0400

    This will be deleted, it is just a test.

---
+++ test.py
@@ -0,0 +1,8 @@
+#This is my first py in atom
+#and now is my first uploaded py to git...
+print ("Hello you dirty bastard. What's your name?")
+user = raw_input(">> ")
+doing = raw_input("And how are you doing? ")
+
+print ("So let me get this straight...")
+print "%s is %s and nobody cares? Unbelievable!" % (user, doing)
